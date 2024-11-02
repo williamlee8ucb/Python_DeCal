30ef04b11968e3d1b4fac373a87696e336372ed4
William Lee <williamlee@visitor-10-56-19-183.wifi.berkeley.edu>
2024-11-01 21:46:27 -0700

    finished HW_05.py

--- Homework/HW_05.py
+++ Homework/HW_05.py
@@ -6,13 +6,11 @@ import numpy as np
 # 1) The Odd Ones Out
 
 def onlyOdd(arr):
-    arr1 = np.array(arr)
-    odd_arrs = np.empty((0, arr1.shape[1]), int)
-    for j in range(len(arr1)):
-        if all(x % 2 == 1 for x in arr1[j]):
-            odd_arrs = np.append(odd_arrs, [arr1[j]], axis = 0)
+    odd_arrs = np.array([])
+    for j in arr:
+        if np.all(j % 2 != 0):
+            odd_arrs = np.append(odd_arrs, [j])
     return odd_arrs
-
 
 arr = np.array([[1, 2, 3], [5, 7, 9], [2, 4, 6], [7, 7, 7]])
 print(onlyOdd(arr))
@@ -40,7 +38,7 @@ def checkerboard1():
 board1 = checkerboard1()
 print(board1)
 
-
+# 2.3:
 def checkerboard2():
     board = np.zeros((8,8))
     for j in range(len(board)):
@@ -55,3 +53,55 @@ def checkerboard2():
     return board
 board2 = checkerboard2()
 print(board2)
+
+# 2.4:
+def reverse_checkerboard():
+    board = np.zeros((8,8))
+    for j in range(len(board)):
+        if j % 2 == 1:
+            for k in range(len(board[j])):
+                if k % 2 == 0:
+                    board[j][k] = 1
+        if j % 2 == 0:
+            for k in range(len(board[j])):
+                if k % 2 == 1:
+                    board[j][k] = 1
+    return board
+
+reverse_board = reverse_checkerboard()
+print(reverse_board)
+
+
+# 3) The Expanding Universe
+
+def expansion(phrase, spaces):
+    expanded_words = []
+    for word in phrase:
+        expanded = ""
+        for char in word:
+            expanded = expanded + char + " " * spaces
+        expanded_words.append(expanded)
+
+    return expanded_words
+
+universe = np.array(["galaxy", "clusters"])
+print(expansion(universe, 2))
+
+
+# 4) 
+
+def secondLargest(planets):
+    planets = np.transpose(planets)
+    list_seconds = []
+    for system in planets:
+        planets.sort()
+        list_seconds.append(system[len(planets)-2])
+    return list_seconds
+
+np.random.seed(42)
+planets = np.random.randint(100, 1000, (5, 5))
+print(planets)
+print(secondLargest(planets))
+
+
+
